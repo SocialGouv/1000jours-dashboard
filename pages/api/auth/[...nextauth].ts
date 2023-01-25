f9ca5eab9cb5a56da70d1faf0087d4f9bb946fe2
Lebret Audrey <alebret@ippon.fr>
2023-01-25 16:53:32 +0100

    feat(auth): intégration de l'auth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,57 @@
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import axios from 'axios'
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "test@test.com" },
+        password: {  label: "Password", type: "password" }
+      },
+    async authorize(credentials) {
+        try {
+          const { data } = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/local`, {
+            identifier: credentials?.email,
+            password: credentials?.password
+          });
+          if (data) {
+            return data;
+          }
+          else {
+            return null;
+          }
+        } catch (e: any) {
+          const errorMessage = e.response.data.message
+          // Redirecting to the login page with error message in the URL
+          throw new Error(errorMessage + '&email=' + credentials?.email)
+        }
+      }
+    })
+  ],
+
+  session: {
+    strategy: 'jwt'
+  },
+
+  callbacks: {
+    async jwt({ token, user, account, profile, isNewUser }) { 
+      if(user) token.user = user
+      if(account) token.account = account
+      if(profile) token.profile = profile
+
+      return token
+    },
+    async session({ session, token, user }) { 
+      if(token) session.token = token
+      
+      if(user) session.user = user
+      else session.user = token.user.user
+
+      return session
+    },
+  },
+
+  debug: true,
+})
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { NextPage } from "next";
 
@@ -6,10 +6,21 @@ import { Alert } from "@codegouvfr/react-dsfr/Alert";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 import Stack from "@mui/material/Stack";
 
-const Home: NextPage = () => {
-  const onClick1 = () => {
-    throw new Error("Hello, sentry");
-  };
+import { signIn, signOut, useSession, getSession } from "next-auth/react";
+import axios from 'axios'
+
+const Home: NextPage = (initialData) => {
+  //const [session, loading] = useSession() // strapi V4
+  const { data: session, status } = useSession() // strapi V3
+
+
+  useEffect(() => {
+    console.log("status : " + status)
+
+    if (status === "authenticated") {
+      console.log("session : " + session)
+    }
+  }, [session, status])
 
   return (
     <>
@@ -22,7 +33,7 @@ const Home: NextPage = () => {
         severity="success"
         title="Message successfully sent"
       />
-      <div className="fr-grid-row fr-grid-row--center fr-grid-row--middle fr-mb-8w fr-mt-8w">
+      {/* <div className="fr-grid-row fr-grid-row--center fr-grid-row--middle fr-mb-8w fr-mt-8w">
         <div className="fr-col-12 fr-col-md-6">
           <h1>
             Template
@@ -32,32 +43,70 @@ const Home: NextPage = () => {
           </h1>
           <p className="fr-mt-10w">
             Pariatur veniam ipsum pariatur elit ullamco sit quis ipsum ad veniam
-            proident sunt. Qui ut irure in quis reprehenderit. Laborum anim ad
-            laboris ipsum magna ullamco consequat ex consectetur. Duis sit
-            adipisicing ipsum occaecat commodo consequat officia ea. Cupidatat
-            fugiat reprehenderit aliqua eiusmod mollit Lorem consectetur. Minim
-            elit proident eu qui exercitation mollit id esse velit et dolore
-            velit laboris. Ipsum occaecat Lorem occaecat magna excepteur veniam
-            ullamco cupidatat irure incididunt velit nulla.
+            proident sunt.
           </p>
         </div>
         <div className="fr-col-12 fr-col-offset-md-1 fr-col-md-4">
-          {/* eslint-disable-next-line jsx-a11y/img-redundant-alt*/}
-          {/* eslint-disable-next-line @next/next/no-img-element*/}
           <img
             className="fr-mt-2w"
             src="https://dummyimage.com/300x300/188cf2/fff.png&amp;text=logo+1"
             alt="My description"
           />
         </div>
+      </div> */}
+      <div>
+        <h1>Auth Test</h1>
+
+        <div>
+          {!session && <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+          </>}
+          {session && <>
+            Signed in as {session?.user?.username} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>}
+        </div>
+
+        <h1>Content...</h1>
+
+        <div>
+          {initialData.articles && initialData.articles.map((each, index) => {
+            return (
+              <div key={index}>
+                <h3>{each.Title}</h3>
+                <p>{each.articles}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
-      <Stack spacing={2} sx={{ mt: 5 }} direction="row">
-        <Button title="nbla" onClick={onClick1}>
-          Trigger sentry error
-        </Button>
-      </Stack>
     </>
   );
 };
+
+
+// TODO: vérifier l'utilité
+export async function getServerSideProps({ req }) {
+  let headers = {}
+  console.log("getServerSideProps")
+  const session = await getSession({ req });
+  if (session) {
+    console.log("getServerSideProps session")
+    headers = { Authorization: `Bearer ${session.jwt}` };
+  }
+  let articles = [];
+  try {
+    let { data } = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/articles`, {
+      headers: headers,
+    })
+    articles = data;
+  } catch (e) {
+    console.log('caught error');
+    articles = [];
+  }
+
+  return { props: { articles: articles } }
+}
 
 export default Home;
